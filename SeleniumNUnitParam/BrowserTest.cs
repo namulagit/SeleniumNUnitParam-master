3ac7973f6a07847db759b44d6c1bdb300fc9ba78
Syed Daud <syed.daud@india-synverse.com>
2017-12-03 19:45:39 +0530

    sorce

---
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNUnitParam
+{
+    public class BrowserTest : Hooks
+    {
+
+
+        [Test]
+        public void LoginCheck()
+        {
+            Driver.Navigate().GoToUrl("http://loaduser1:Vanadium2200@beacon-test/sorce/");
+            System.Threading.Thread.Sleep(5000);
+            TimeSpan pageloadwait = TimeSpan.FromSeconds(30.00);
+            Driver.Manage().Timeouts().ImplicitWait = pageloadwait;
+            string title = Driver.Title;
+            Assert.AreEqual("Homepage", title, "Page Title");
+            System.Threading.Thread.Sleep(3000);
+            Driver.Close();
+        }
+
+    }
+}
